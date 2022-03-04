faab2191cb9f37ad4ca570c78fdaf7c442c9330a
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-03 19:57:49 -0600

    Exception: agregando exception a handler de tareas finalizadas

--- src/main/java/com/vaesco/todoapp/service/TaskService.java
+++ src/main/java/com/vaesco/todoapp/service/TaskService.java
@@ -1,14 +1,17 @@
 package com.vaesco.todoapp.service;
 
+import com.vaesco.todoapp.exception.TodoExceptions;
 import com.vaesco.todoapp.mapper.TaskInDTOToTask;
 import com.vaesco.todoapp.persistence.entity.Task;
 import com.vaesco.todoapp.persistence.entity.TaskStatus;
 import com.vaesco.todoapp.persistence.repository.TaskRepository;
 import com.vaesco.todoapp.service.dto.TaskInDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -42,6 +45,11 @@ public class TaskService {
 
     @Transactional
     public void updateTaskFinished(Long id) {
+        Optional<Task> optionalTask = this.repository.findById(id);
+        if (optionalTask.isEmpty()) {
+            throw new TodoExceptions("Task not found", HttpStatus.NOT_FOUND);
+        }
+
         this.repository.updateTaskAndFinished(id);
     }
 
